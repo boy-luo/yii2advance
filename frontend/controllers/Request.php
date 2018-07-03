a7852b73c177c3735e35a921ac1093709742e8f2
dzluo <luo@dz.com>
2018-07-03 11:26:47 +0800

    新增请求处理  和 restful控制器

---
+++ frontend/controllers/Request.php
@@ -0,0 +1,90 @@
+<?php
+namespace frontend\controllers;
+
+use Yii;
+use yii\base\InvalidParamException;
+use yii\web\BadRequestHttpException;
+use yii\web\Controller;
+use yii\filters\VerbFilter;
+use yii\filters\AccessControl;
+use common\models\LoginForm;
+use frontend\models\PasswordResetRequestForm;
+use frontend\models\ResetPasswordForm;
+use frontend\models\SignupForm;
+use frontend\models\ContactForm;
+
+/**
+ * 请求处理（Handling Requests）:
+ */
+class Request extends Controller
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function behaviors()
+    {
+        return [
+            'access' => [
+                'class' => AccessControl::className(),
+                'only' => ['logout', 'signup'],
+                'rules' => [
+                    [
+                        'actions' => ['signup'],
+                        'allow' => true,
+                        'roles' => ['?'],
+                    ],
+                    [
+                        'actions' => ['logout'],
+                        'allow' => true,
+                        'roles' => ['@'],
+                    ],
+                ],
+            ],
+            'verbs' => [
+                'class' => VerbFilter::className(),
+                'actions' => [
+                    'logout' => ['post'],
+                ],
+            ],
+        ];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function actions()
+    {
+        return [
+            'error' => [
+                'class' => 'yii\web\ErrorAction',
+            ],
+            'captcha' => [
+                'class' => 'yii\captcha\CaptchaAction',
+                'fixedVerifyCode' => YII_ENV_TEST ? 'testme' : null,
+            ],
+        ];
+    }
+
+    /**
+     * Displays homepage.
+     *
+     * @return mixed
+     */
+    public function actionIndex()
+    {
+        // $headers 是一个 yii\web\HeaderCollection 对象
+        $headers = Yii::$app->request->headers;
+        var_dump($headers);
+
+        // 返回 Accept header 值
+        $accept = $headers->get('Accept');
+        var_dump($accept);
+
+        if ($headers->has('User-Agent')) { /* 这是一个 User-Agent 头 */ }
+        var_dump($headers->get('User-Agent'));
+
+
+        // return $this->render('study');
+    }
+
+}
